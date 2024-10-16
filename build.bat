a5ad8b656caa3202ec4e7233214a4df2a0f84987
saacidyuusuf <saacidyuusuf871@gmail.com>
2024-10-16 05:35:59 -0400

    added little

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Users\K C T\AppData\Local\Programs\Python\Python313\Scripts\pyinstaller.exe" --onefile --icon=word.ico payload.py
+"C:\Users\K C T\AppData\Local\Programs\Python\Python313\Scripts\pyinstaller.exe" --onefile  payload.py
